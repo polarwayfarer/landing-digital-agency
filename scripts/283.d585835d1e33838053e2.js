bc34c721770a3e9ad115463ac04f4468ae7348fd
Yanka_InCode <yankaincode@gmail.com>
2022-09-05 19:34:02 +0300

    Updates

---
+++ scripts/283.d585835d1e33838053e2.js
@@ -0,0 +1,2 @@
+(self.webpackChunkfrom_figma_digital_agency=self.webpackChunkfrom_figma_digital_agency||[]).push([[283],{283:function(){document.body;var e=document.querySelector(".page-on-load-container");window.addEventListener("load",(function n(){setTimeout((function(){e.classList.add("page-on-load-container--fade-out")}),3500),setTimeout((function(){e.remove(),window.removeEventListener("load",n,{passive:!0})}),4500)}),{passive:!0})}}]);
+//# sourceMappingURL=283.d585835d1e33838053e2.js.map
