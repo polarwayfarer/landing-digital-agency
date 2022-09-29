dbd274428e2ff9b55b3ac08d9bb6c3676017c9af
Yanka_InCode <yankaincode@gmail.com>
2022-09-29 03:47:09 +0300

    Updates

---
+++ scripts/283.c2734e27e7ba13a7ec8e.js
@@ -0,0 +1,2 @@
+(self.webpackChunkfrom_figma_digital_agency=self.webpackChunkfrom_figma_digital_agency||[]).push([[283],{283:function(){var o=document.body,e=document.querySelector(".page-on-load-container");window.addEventListener("load",(function n(){0!==window.scrollTop&&window.scrollTo(0,0);setTimeout((function(){e.classList.add("page-on-load-container--fade-out")}),3500),setTimeout((function(){o.style.overflowY="auto",e.remove(),window.removeEventListener("load",n,{passive:!0}),o.style.overflowY="auto"}),4500)}),{passive:!0})}}]);
+//# sourceMappingURL=283.c2734e27e7ba13a7ec8e.js.map
